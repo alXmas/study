30a7fc7b80b5b6069dcc8a7767312cf84c6e747d
alXmas <alexsandr.xmas@gmail.com>
2018-12-21 12:04:01 +0400

    Array tests were passed successfully

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,23 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array.map { |elem| elem.positive? ? array.max : elem }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        left = 0
+        right = array.length - 1
+        while left <= right
+          mid = left + (right - left) / 2
+          return mid if array[mid] == query
+
+          if array[mid] > query
+            right = mid - 1
+          else
+            left = mid + 1
+          end
+        end
+        -1
       end
     end
   end
